7cbe2a296833d8dee6eed456ed6fc09b1b5bbb52
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2017-11-27 18:35:14 +0000

    Added Approval test to verify output from Converter class

---
+++ TextFileConverter/TextFileConverter.Console/Tests/ApprovalTest.cs
@@ -0,0 +1,23 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Text;
+using TextFileConverter.Console;
+
+namespace TextFileConverter.Console.Tests
+{
+    [TestFixture]
+    [UseReporter(typeof(NUnitReporter))]
+    public class ApprovalTest
+    {
+        [Test]
+        public void VerifyFixedWidthOutput()
+        {
+            var converter = new Converter($"{TestContext.CurrentContext.TestDirectory}\\Files\\input.csv");
+            converter.ConvertCsvToFixedWidth($"{TestContext.CurrentContext.TestDirectory}\\Files\\output.txt");
+            Approvals.Verify(converter.Output);
+        }
+    }
+}
